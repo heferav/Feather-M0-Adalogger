68ffb1bf854bb31e34d55c62a8f7ba354c417d4e
Andre Hefer <andre@hefer.org.uk>
2016-08-16 17:00:12 +0100

    Some tweaks to read date/time from external RTC on feathering (adalogger)

--- SimpleLogger/SimpleLogger.ino
+++ SimpleLogger/SimpleLogger.ino
@@ -16,45 +16,47 @@
 
 #include <SPI.h>
 #include <SD.h>
-#include <RTCZero.h>
+#include "RTClib.h"  // using adalogger RTC
+#include <RTCZero.h> // using M0 onboard RTC
 
 
-//#define cardSelect 4  // Set the pins used
-#define cardSelect 10  // Set (Adalogger) the pins used
+#define cardSelect 10  // Set (Adalogger) pin [Changed from 4]
 #define VBATPIN A7    // Battery Voltage on Pin A7
-/*
+
+/* Not required for Feather M0 + adalogger
 #ifdef ARDUINO_SAMD_ZERO
    #define Serial SerialUSB
 #endif
 ********************/
 
 File logfile;   // Create file object
+RTC_PCF8523 rtcWNG; // Create RTC object on the adalogger featherwing
+RTCZero rtcM0;    // Create RTC object for Feather M0 onboard RTC
 
-RTCZero rtc;    // Create RTC object
-
-/* Change these values to set the current initial time */
+/*
+// Change these values to set the current initial time
 const byte hours = 16;
 const byte minutes = 23;
 const byte seconds = 0;
-/* Change these values to set the current initial date */
+// Change these values to set the current initial date
 const byte day = 15;
 const byte month = 8;
 const byte year = 16;
-
+*/
 
 //////////////    Setup   ///////////////////
 void setup() {
 
-  rtc.begin();    // Start the RTC
-  rtc.setTime(hours, minutes, seconds);   // Set the time
-  rtc.setDate(day, month, year);    // Set the date
+  rtcM0.begin();    // Start the RTC
+  //rtcM0.setTime(hours, minutes, seconds);   // Set the time
+  //rtcM0.setDate(day, month, year);    // Set the date
   
   /* If you want output for debugging and the Feather to wait for you
   to open a serial port uncomment the the lines below.
   */
-  // while (! Serial); // Wait until Serial is ready
-  // Serial.begin(115200);
-  // Serial.println("\r\nAnalog logger test");
+   while (! Serial); // Wait until Serial is ready
+   Serial.begin(115200);
+   Serial.println("\r\nAnalog logger test");
   
   pinMode(13, OUTPUT);
 
@@ -88,6 +90,25 @@ void setup() {
   //pinMode(8, OUTPUT);
   pinMode(12, OUTPUT);
   Serial.println("Ready!");
+
+   //******************* START RTC_PCF8523 INITIAL SETUP *********************
+  if (! rtcWNG.begin()) {
+    Serial.println("Couldn't find RTC (Adalogger)");
+    while (1);
+  }
+  //******************* END RTC_PCF8523 INITIAL SETUP ***********************
+
+  Serial.println("Before syncing to featherwing RTC: ");
+  show_M0_rtc_date_and_time();
+  show_adalogger_rtc_date_and_time();
+  //Read featherwing RTC and set onboard RTC to match
+  DateTime now = rtcWNG.now(); // Read Adalogger RTC and use it to set featherwing M0 RTC
+  rtcM0.setTime(now.hour(), now.minute(), now.second());
+  rtcM0.setDate(now.day(), now.month(), now.year() - 2000);
+
+  Serial.println("After syncing to featherwing RTC: ");
+  show_M0_rtc_date_and_time();
+  show_adalogger_rtc_date_and_time();
 }
 
 uint8_t i=0;
@@ -102,29 +123,20 @@ void loop() {
   measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
   measuredvbat /= 1024; // convert to voltage
   
-  /* Print RTC Time
-  Serial.print(rtc.getHours());
-  Serial.print(":");
-  Serial.print(rtc.getMinutes());
-  Serial.print(":");
-  Serial.print(rtc.getSeconds());
-  Serial.print(",");
-  Serial.println(measuredvbat);   // Print battery voltage
-  */
-  
-  // Print data and time followed by battery voltage to SD card
+    
+  // Print data and RTC (M0) time followed by battery voltage to SD card
   logfile.print("20"); //YYYY year format
-  logfile.print(rtc.getYear());
+  logfile.print(rtcM0.getYear());
   logfile.print("/");
-  print2digits(rtc.getMonth());
+  print2digits_F(rtcM0.getMonth());
   logfile.print("/");
-  print2digits(rtc.getDay());
+  print2digits_F(rtcM0.getDay());
   logfile.print("\t");
-  print2digits(rtc.getHours());
+  print2digits_F(rtcM0.getHours());
   logfile.print(":");
-  print2digits(rtc.getMinutes());
+  print2digits_F(rtcM0.getMinutes());
   logfile.print(":");
-  print2digits(rtc.getSeconds());
+  print2digits_F(rtcM0.getSeconds());
   logfile.print(", ");
   logfile.println(measuredvbat);   // Print battery voltage
   logfile.flush();
@@ -153,10 +165,51 @@ void error(uint8_t errno) {
   }
 }
 
-//pad out date/time fields two two digits
-void print2digits(int number) {
+//Padding date/time fields two two digits -- to logfile
+void print2digits_F(int number) {
   if (number >= 0 && number < 10) {
     logfile.print('0');
   }
   logfile.print(number);
+}
+
+//Print onboard RTC date/time to terminal
+void show_M0_rtc_date_and_time(){   
+    Serial.print("Feather M0 onboard RTC : 20");
+    Serial.print(rtcM0.getYear());
+    Serial.print("/");
+    print2digits(rtcM0.getMonth());
+    Serial.print("/");
+    print2digits(rtcM0.getDay());
+    Serial.print("\t");
+    print2digits(rtcM0.getHours());
+    Serial.print(":");
+    print2digits(rtcM0.getMinutes());
+    Serial.print(":");
+    print2digits(rtcM0.getSeconds());
+    Serial.println();   
+}
+
+void show_adalogger_rtc_date_and_time(){   
+    DateTime now = rtcWNG.now();
+    Serial.print("Adalogger RTC (PCF8523): ");
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    print2digits(now.month());
+    Serial.print('/');
+    print2digits(now.day());
+    Serial.print("\t");
+    print2digits(now.hour());
+    Serial.print(':');
+    print2digits(now.minute());
+    Serial.print(':');
+    print2digits(now.second());
+    Serial.println();  
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
 }
