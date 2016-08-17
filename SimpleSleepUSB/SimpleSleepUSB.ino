5ded1a13286666a51c7a453259b1fe3fed2c3d2e
Andre Hefer <andre@hefer.org.uk>
2016-08-17 08:53:13 +0100

    commented out all serial comms. Goes to sleep and wakes up fine.

--- SimpleSleepUSB/SimpleSleepUSB.ino
+++ SimpleSleepUSB/SimpleSleepUSB.ino
@@ -4,61 +4,96 @@
   By: CaveMoa
   Date: 30/12/15
 */
-/* Not required on Feather M0
-#define Serial SerialUSB
-*/
+
+//Arduino ZERO needs this. Featherwing send SERIAL to USB by default
+#if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
+  // Required for Serial on Zero based boards
+  #define Serial SERIAL_PORT_USBVIRTUAL
+#endif
+
 #include <RTCZero.h>
 
 
 /* Create an rtc object */
-RTCZero rtc;
+RTCZero rtcM0;
 int AlarmTime;
 
 void setup()
 {
-  rtc.begin();
-  Serial.begin(9600);
-  while (! Serial); // Wait until Serial is ready
-  Serial.println("Ready!");
-  Serial.end();
+  rtcM0.begin();
+  //Serial.begin(9600);
+  //while (! Serial); // Wait until Serial is ready
+  //Serial.println("Ready!");
+  //show_M0_rtc_date_and_time();
+  //Serial.end();
 }
 
 void loop()
 {
-  
-  AlarmTime = rtc.getSeconds()+10; // Adds 10 seconds to alarm time
+  pinMode(12, INPUT_PULLUP); // turns the LED on   
+  delay(1000); 
+  pinMode(12, INPUT); // turns the LED off
+  AlarmTime = rtcM0.getSeconds()+5; // Adds 5 seconds to alarm time
   AlarmTime = AlarmTime % 60; // checks for roll over 60 seconds and corrects
-  Serial.print("Next Alarm Time:");
-  Serial.println(AlarmTime);
+  show_M0_rtc_date_and_time();
+  //Serial.print("Next Alarm Time (seconds):");
+  //Serial.println(AlarmTime);
   
-  rtc.setAlarmSeconds(AlarmTime); // Wakes at next alarm time
-  rtc.enableAlarm(rtc.MATCH_SS); // Match seconds only
-  rtc.attachInterrupt(alarmMatch);
+  rtcM0.setAlarmSeconds(AlarmTime); // Wakes at next alarm time
+  rtcM0.enableAlarm(rtcM0.MATCH_SS); // Match seconds only
+  rtcM0.attachInterrupt(alarmMatch);
   
-  Serial.end();
+  //Serial.end();
   USBDevice.detach(); // Safely detach the USB prior to sleeping
   
-  rtc.standbyMode();    // Sleep until next alarm match
+  rtcM0.standbyMode();    // Sleep until next alarm match
 
   USBDevice.attach();   // Re-attach the USB, audible sound on windows machines
 
   // Simple indication of being awake
-  digitalWrite(13, HIGH);   // turn the LED on 
-  delay(100);              
-  digitalWrite(13, LOW);    // turn the LED off
-  delay(100);
-  digitalWrite(13, HIGH);   // turn the LED on 
-  delay(100);              
-  digitalWrite(13, LOW);    // turn the LED off
+  
+  pinMode(12, INPUT_PULLUP); // turns the LED on   
+  delay(1000); 
+  pinMode(12, INPUT); // turns the LED off
+  pinMode(12, INPUT_PULLUP); // turns the LED on   
+  delay(1000); 
+  pinMode(12, INPUT); // turns the LED off
+  pinMode(12, INPUT_PULLUP); // turns the LED on   
+  delay(1000); 
+  pinMode(12, INPUT); // turns the LED off
 
   delay(1000);  // Delay added to make serial more reliable
   
-  Serial.begin(9600);
+  //Serial.begin(9600);
   //while (! Serial); // Wait until Serial is ready
-  Serial.println("Awake");  
+  //Serial.println("Awake");  
 }
 
 void alarmMatch() // Do something when interrupt called
 {
   
+}
+
+//Print onboard RTC date/time to terminal
+void show_M0_rtc_date_and_time(){   
+    Serial.print("Feather M0 onboard RTC : 20");
+    Serial.print(rtcM0.getYear());
+    Serial.print("/");
+    print2digits(rtcM0.getMonth());
+    Serial.print("/");
+    print2digits(rtcM0.getDay());
+    Serial.print("\t");
+    print2digits(rtcM0.getHours());
+    Serial.print(":");
+    print2digits(rtcM0.getMinutes());
+    Serial.print(":");
+    print2digits(rtcM0.getSeconds());
+    Serial.println();   
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
 }
